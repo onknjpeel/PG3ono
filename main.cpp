ff2b5cfcee564dbeea1a77dc7b3ae3a5a7830c45
onknjpeel <k023g0103@g.neec.ac.jp>
2024-10-16 16:09:00 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <Windows.h>
 
+int Recursive(int time) {
+	if (time <= 1) {
+		return 100;
+	}
+	return ((time * 100 - 50) + Recursive(time - 1));
+}
+
+
 int main() {
 	SetConsoleOutputCP(65001);
+	int time = 22;
+	int Rmoney = Recursive(time);
 
-	printf("ああああ");
+	int Nmoney = 1072 * time;
 
-	return 0; 
+	printf("Recursive:%d\nNormal:%d", Rmoney, Nmoney);
+
+	return 0;
 }
